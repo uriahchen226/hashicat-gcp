22e09b8c31e08a9234baaa434f4596c45adb8e45
Uriah Chen <uriah91028@gmail.com>
2021-06-15 09:44:21 +0000

    Added vpc module

--- main.tf
+++ main.tf
@@ -46,8 +46,6 @@ resource "tls_private_key" "ssh-key" {
 resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
-  department   = "devops"
-  billable     = "true"
   machine_type = var.machine_type
 
   boot_disk {
@@ -70,6 +68,8 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    department   = "devops"
+    billable     = "true"
   }
 
 }
