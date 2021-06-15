20408d55a7a2a34245f4f986548edaa29d0163f6
Uriah Chen <uriah91028@gmail.com>
2021-06-15 09:04:16 +0000

    Added the second label

--- main.tf
+++ main.tf
@@ -47,6 +47,7 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   department   = "devops"
+  billable     = "true"
   machine_type = var.machine_type
 
   boot_disk {
